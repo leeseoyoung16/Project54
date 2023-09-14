9a3b77f2c7da7e29be09b6f125247e8795de7c34
tjdud <tjdud@DESKTOP-7KJFUDJ>
2023-09-14 10:41:49 +0900

    0914 실습

--- Project54/main.c
+++ Project54/main.c
@@ -2,16 +2,39 @@
 #include<stdlib.h>
 #include<memory.h>
 
-//주어진 트리에 대하여 전위 중위 후위로 링크,배열
+//주어진 트리에 대하여 전위 중위 후위로 링크,배열 (01과제)
+//스택과 링크 표현법을 이용한 전위,중위, 후위순회(02과제)
 
-//링크를 이용한 순회
+#define SIZE 100
 
 typedef struct TreeNode {
 	int data;
 	struct TreeNode* left, * right;
 }TreeNode;
 
-TreeNode n1 = {4,NULL,NULL};
+
+//stack을 이용한 순회
+
+int top = -1;
+TreeNode* stack[SIZE];
+
+
+void push(TreeNode* item) {
+    if (top < SIZE - 1) {
+        stack[++(top)] = item;
+    }
+}
+
+TreeNode* pop() {
+    TreeNode* item = NULL;
+    if (top >= 0) {
+        item = stack[top--];
+    }
+    return item;
+}
+
+
+TreeNode n1 = {4,NULL,NULL};   
 TreeNode n2 = {5,NULL,NULL};
 TreeNode n3 = {3,&n1,&n2 };
 TreeNode n4 = {6,NULL,NULL };
@@ -24,12 +47,13 @@ TreeNode n10 = {7,&n6,&n9 };
 TreeNode n11 = {1,&n5,&n10 };
 TreeNode* root = &n11;
 
+//링크를 이용한 순회
 // 전위 순회 함수
 void preorder(TreeNode* root) {
     if (root != NULL) {
         printf("%d ", root->data);
-        preorder(root->left);
-        preorder(root->right);
+        preorder(root->left); 
+        preorder(root->right); 
 
     }
 }
@@ -37,16 +61,16 @@ void preorder(TreeNode* root) {
 void inorder(TreeNode* root) {
     if (root != NULL) {
         inorder(root->left);
-        printf("%d ", root->data);
-        inorder(root->right);
+        printf("%d ", root->data); 
+        inorder(root->right); 
     }
 }
 //후위 순회
 void postorder(TreeNode* root) {
     if (root != NULL) {
         postorder(root->left);
-        postorder(root->right);
-        printf("%d ", root->data);
+        postorder(root->right); 
+        printf("%d ", root->data); 
         
     }
 }
@@ -83,6 +107,72 @@ void postorderToArray(TreeNode* node) {
         resultArray[currentIndex++] = node->data; 
     }
 }
+// 스택을 사용한 전위 순회 함수
+void stackPreorder(TreeNode* root) {
+    if (root == NULL) return;
+
+    push(root);
+
+    while (top >= 0) {
+        TreeNode* current = pop();
+        printf("%d ", current->data);
+
+        if (current->right != NULL) {
+            push(current->right);
+        }
+
+        if (current->left != NULL) {
+            push(current->left);
+        }
+    }
+}
+
+// 스택을 사용한 중위 순회 함수
+void stackInorder(TreeNode* root) {
+    if (root == NULL) return;
+
+    TreeNode* current = root;
+
+    while (current != NULL || top >= 0) {
+        while (current != NULL) {
+            push(current);
+            current = current->left;
+        }
+
+        current = pop();
+        printf("%d ", current->data);
+        current = current->right;
+    }
+}
+
+// 스택을 사용한 후위 순회 함수
+void stackPostorder(TreeNode* root) {
+    if (root == NULL) return;
+
+    TreeNode* current = root;
+    TreeNode* lastVisited = NULL;
+
+    while (current != NULL || top >= 0) {
+        while (current != NULL) {
+            push(current);
+            current = current->left;
+        }
+
+        current = stack[top];
+
+        if (current->right == NULL || current->right == lastVisited) {
+            printf("%d ", current->data);
+            pop();
+            lastVisited = current;
+            current = NULL;
+        }
+        else {
+            current = current->right;
+        }
+    }
+}
+
+
 
 int main(void) {
 
@@ -125,6 +215,18 @@ int main(void) {
     }
 
     printf("\n");
+
+    printf("\n<Traversal with Stack>\n\n"); //Traversal with Stack 출력
+    printf("1.전위 순회= ");
+    stackPreorder(root);
+    printf("\n");
+    printf("2.중위 순회= ");
+    stackInorder(root);
+    printf("\n");
+    printf("3.후위 순회= ");
+    stackPostorder(root);
+    printf("\n");
+
 
     return 0;
 
